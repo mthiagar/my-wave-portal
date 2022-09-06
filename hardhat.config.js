aef8007dceb80e2b2c154a35ac5d6382619fe213
Madhuran Thiagarajah <madhuran.v@gmail.com>
2022-09-05 21:09:14 -0400

    add config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
